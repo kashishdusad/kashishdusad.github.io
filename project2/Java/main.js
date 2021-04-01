35e8a40295ae18dbb8e72a97760d7d7098595c4f
kashishdusad <dusak292@newschool.edu>
2021-04-01 06:03:54 +0530

    Dom edit

--- project2/Java/main.js
+++ project2/Java/main.js
@@ -1,20 +1,22 @@
 var container = document.querySelector('.container');
-var clear = document.querySelector('.clear')
-var button = document.querySelector('button')
-var count = document.querySelector('#counter')
-var yellowb = document.querySelector('.yellow')
-var orangeb = document.querySelector('.orange')
-var greenb = document.querySelector('.green')
-var blueb = document.querySelector('.blue')
-var pinkb = document.querySelector('.pink')
+var clear = document.querySelector('.clear');
+var button = document.querySelector('button');
+var count = document.querySelector('.display_count');
+// var yellowb = document.querySelector('.yellow');
+// var orangeb = document.querySelector('.orange');
+// var greenb = document.querySelector('.green');
+// var blueb = document.querySelector('.blue');
+// var pinkb = document.querySelector('.pink');
+var click = document.querySelector('.click');
 
+// Its adding two entries on click and not appearing random even though I used the same code as the tutorial (matbe its because i'm using flexbox)
+click.addEventListener('click', function (event) {
+    let shape = document.createElement('div');
+    shape.classList.add('yellow');  shape.style.left = (90 * Math.random()) + '%';
+    // yellow.style.top = (90 * Math.random()) + '%';
+    container.appendChild(shape);
+});
 
-for (let i = 0; i < 50; i++) {
-  let yellow = document.createElement('div');
-  yellow.classList.add('yellow');  yellow.style.left = (90 * Math.random()) + '%';
-  yellow.style.top = (90 * Math.random()) + '%';
-  container.appendChild(yellow);
-};
 
 container.addEventListener('click', function (event) {
   if (event.target.classList.contains('yellow')) {
@@ -27,3 +29,19 @@ clear.addEventListener('click',function(){
     count.innerHTML='0';
     alert("Are you sure you want to clear?");
 })
+// Tried multiple ways to get the count but it doesn't work
+  function count() {
+  var nodelist = document.getElementById("yellow");
+  // var nodelist = div.getElementsByTagName("div");
+  document.getElementById("display_count").innerHTML = nodelist.length;
+  console.log(nodelist.length);
+}
+// Tried multiple ways to get the colours changed but it doesn't work
+
+// $(document).ready(function() {
+
+  $('.orangeb').on('click', function(){
+      $('orangeb').toggleClass('yellow');
+  });
+  console.log('You can click!');
+// });
