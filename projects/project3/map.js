d5e0d4bbbd901c9e5994633630e869b57aff6573
kashishdusad <dusak292@newschool.edu>
2021-04-09 18:43:23 +0530

    Index EC

---
+++ projects/project3/map.js
@@ -0,0 +1,63 @@
+const SCREEN_SIZE = Math.min(640, window.innerWidth);
+let points = [];
+let savedPixels;
+let img;
+let slider;
+
+function preload() {
+    img = loadImage('https://raw.githubusercontent.com/florinpop17/work-journal/master/Projects/006%20-%20Dotted%20World%20Map/map.jpg');
+}
+
+function setup() {
+    // Create slider and add change event
+    slider = createSlider(1, 10, 3, 1);
+    slider.changed(() => {
+        drawImageFromPoints();
+    });
+
+    // Create canvas
+    createCanvas(SCREEN_SIZE, SCREEN_SIZE * 3 / 4);
+	pixelDensity(1);
+
+    // Get image and center it on the screen
+    imageMode(CENTER);
+    img.resize(SCREEN_SIZE, SCREEN_SIZE);
+    image(img, width / 2, height / 2);
+
+    // Save pixels
+    loadPixels();
+    drawImageFromPoints();
+}
+
+function getPoints(sliderValue) {
+    // reset points array
+    points = []
+
+    // Save all points
+    for(let x = 0; x < width; x+= sliderValue) {
+        for(let y = 0; y < height; y+= sliderValue) {
+            let index = (x + y * width) * 4;
+
+            // Only get the non-white values
+            if(pixels[index] < 200) {
+                points.push({
+                    x,
+                    y
+                });
+            }
+        }
+    }
+}
+
+function drawImageFromPoints() {
+    let sliderValue = slider.value();
+
+    getPoints(sliderValue);
+
+    background(0);
+    points.forEach(p => {
+        noStroke();
+        fill(255);
+        ellipse(p.x, p.y, sliderValue, sliderValue);
+    });
+}
